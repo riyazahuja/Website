1a73a9a4e4bb4808e152736a255359a0f8d6ebb9
riyazahuja <riyaza@andrew.cmu.edu>
2024-06-30 03:17:52 -0400

    Update PostDataPage.js

--- src/components/PostDataPage.js
+++ src/components/PostDataPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBox from './NavBox';
-import ImageSwiper2 from './ImageSwiper2';
 import parse from 'html-react-parser';
 
 function PostDataPage({ darkMode }) {
